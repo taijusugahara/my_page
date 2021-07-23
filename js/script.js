da97c5230d5b8f4ff128df659c284571a6a816ae
taijusugahara <80800469+taijusugahara@users.noreply.github.com>
2021-07-23 10:41:03 +0900

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -6,6 +6,7 @@ $(function(){
 });
 
 // トップへ戻るボタン
+// headerにiフォントを読み込む記述しないと映らないので注意
 var pageTop = $("#page-top");
 // ボタン非表示
 pageTop.hide();
@@ -34,41 +35,93 @@ $('a[href^="#"]').click(function(){
   var target = $(href == "#" || href == "" ? 'html' : href);
   var position = target.offset().top;
   $("html, body").animate({scrollTop:position-85}, speed, "swing");
+      $('.nav-btn').removeClass('-active');
+      $('.burger_links').removeClass('-active');
   return false;
 });
 
 
+// $(function() {
+//   var rotate = $(".skil-image");
+//   var click = false;
+//   var roop;
+//   var count = 0;
+
+//   //回転させる
+//   function start(){
+//       roop = setInterval(function(){
+//           count++;
+//           if(count > 360){
+//               count = 0;
+//           }else{
+//               rotate.css("transform", "rotate("+ count +"deg)");
+//           }
+//       }, 10);
+//   }
+
+//   //クリックされた時の処理
+//   rotate.click(function(){
+//       toggle();
+//   });
+
+//   //ループ中かどうか判断する
+//   function toggle(){
+//       if(click){
+//           clearInterval(roop);
+//           click = false;
+//       }else{
+//           start();
+//           click = true;
+//       }
+//   }
+// });
+
+
+
+
+
+// 動画横幅調整
 $(function() {
-  var rotate = $(".skil-image");
-  var click = false;
-  var roop;
-  var count = 0;
+  var getWindowMovieHeight = function() {
+    // ここでブラウザの縦横のサイズを取得します。
+    var windowSizeHeight = $(window).outerHeight();
+    var windowSizeWidth = $(window).outerWidth();
 
-  //回転させる
-  function start(){
-      roop = setInterval(function(){
-          count++;
-          if(count > 360){
-              count = 0;
-          }else{
-              rotate.css("transform", "rotate("+ count +"deg)");
-          }
-      }, 10);
-  }
+    // メディアの縦横比に合わせて数値は変更して下さい。(メディアのサイズが width < heightの場合で書いています。逆の場合は演算子を逆にしてください。)
+                                                    // width>heightだったけどこの記述で上手くったよ
+    var windowMovieSizeWidth = windowSizeHeight * 1.76388889;
+    var windowMovieSizeHeight = windowSizeWidth / 1.76388889;
+    var windowMovieSizeWidthLeftMargin = (windowMovieSizeWidth - windowSizeWidth) / 2;
 
-  //クリックされた時の処理
-  rotate.click(function(){
-      toggle();
+    if (windowMovieSizeHeight < windowSizeHeight) {
+      // 横幅のほうが大きくなってしまう場合にだけ反応するようにしています。
+      $("#video").css({left: -windowMovieSizeWidthLeftMargin});
+    }
+  };
+
+  // 以下画面の可変にも対応できるように。
+  $(window).on('load', function(){
+    getWindowMovieHeight();
   });
 
-  //ループ中かどうか判断する
-  function toggle(){
-      if(click){
-          clearInterval(roop);
-          click = false;
-      }else{
-          start();
-          click = true;
-      }
-  }
+  $(window).on('resize', function(){
+    getWindowMovieHeight();
+  });
+});
+
+// animation / wow.js
+$(function(){
+  new WOW().init();
+});
+
+// works開く
+$(function(){
+  $('.web1').on('click',function(){
+    $('.web_site').toggleClass('-active');
+  });
+});
+$(function(){
+  $('.web2').on('click',function(){
+    $('.web_app').toggleClass('-active');
+  });
 });
